a0db0658bb6b7d5f08985cb7384206454c2e2b49
Jean <55899462+Jean-H77@users.noreply.github.com>
2025-05-05 16:30:55 -0700

    Backend initial commit

---
+++ Packages/Backend/eShop-server/src/main/java/org/server/eshopserver/EShopServerApplication.java
@@ -0,0 +1,13 @@
+package org.server.eshopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EShopServerApplication.class, args);
+    }
+
+}
