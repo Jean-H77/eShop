cb0d432121f5692bb43e0659e1690f7ed0d4a665
Jean <55899462+Jean-H77@users.noreply.github.com>
2025-05-05 23:23:16 -0700

    refactor: introduce BASE_URL into useGetAllShopItems.ts

--- Packages/Frontend/eShop-client/src/pages/shop-page/hooks/useGetAllShopItems.ts
+++ Packages/Frontend/eShop-client/src/pages/shop-page/hooks/useGetAllShopItems.ts
@@ -1,10 +1,12 @@
 import {useQuery} from "@tanstack/react-query";
 import type {ShopItem} from "../types.ts";
 
+const BASE_URL = "http://localhost:8080";
+
 export const useGetAllShopItems = () => {
     return useQuery<ShopItem[]>({
         queryKey: ['shopItems'],
         queryFn: () =>
-            fetch('http://localhost:8080/api/items').then(res => res.json()),
+            fetch(`${BASE_URL}/api/items`).then(res => res.json()),
     });
 }
